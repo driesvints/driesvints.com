876b1957f18072e54e0a00c19e980201aa05fa6b
Dries Vints <dries.vints@gmail.com>
2013-06-28 21:47:49 +0200

    Separate HomeController
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php namespace DriesVints\Controllers;
+
+class HomeController extends BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return 'Hello, World!';
+	}
+
+}
--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,4 @@
 <?php
 
-Route::get('/', array('as' => 'home', function()
-{
-	return 'Hello, World!';
-}));
-
+Route::get('/', array('as' => 'home', 'uses' => 'DriesVints\\Controllers\\HomeController@index'));
 Route::resource('posts', 'DriesVints\\Controllers\\PostsController');
