f6deed522423e1c00e9e4938675729260d738df5
Dries Vints <dries.vints@gmail.com>
2013-06-28 19:41:43 +0200

    Add local dev environment
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,7 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('MacBook-Air-of-Dries-Vints-2.local', '*.loc'),
 
 ));
 
