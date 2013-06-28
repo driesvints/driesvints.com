4f764494a1fb1cc791d6d5aea19383dc7cbf5eed
Dries Vints <dries.vints@gmail.com>
2013-06-28 20:02:03 +0200

    Add posts migration, model, route and blank controller
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

---
+++ app/database/migrations/2013_06_28_173019_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function($table)
+		{
+			$table->increments('id');
+			$table->string('slug')->unique();
+			$table->string('title');
+			$table->text('body');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
---
+++ app/models/Post.php
@@ -0,0 +1,14 @@
+<?php namespace DriesVints\Models;
+
+use Eloquent;
+
+class Post extends Eloquent {
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'posts';
+
+}
