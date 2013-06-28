fecc8a395f7a5a27584d6857212109d5e3c4cbd5
Dries Vints <dries.vints@gmail.com>
2013-06-28 19:44:04 +0200

    Add namespaces to controllers and models
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace DriesVints\Controllers;
 
 class BaseController extends Controller {
 
--- app/models/User.php
+++ app/models/User.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace DriesVints\Models;
 
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
