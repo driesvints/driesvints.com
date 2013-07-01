a1695a80d21b9d08e63068038a6eb406f5653516
Dries Vints <dries.vints@gmail.com>
2013-07-01 19:18:01 +0200

    Re-arrange blog route

--- app/routes.php
+++ app/routes.php
@@ -17,6 +17,8 @@ Route::model('blog', 'Models\\Post');
 
 Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
 
+Route::resource('blog', 'BlogController', array('only' => array('index', 'show')));
+
 Route::get('login', array('as' => 'login', 'uses' => 'AuthController@getLogin'));
 Route::post('login', 'AuthController@postLogin');
 Route::get('logout', array('as' => 'logout', 'uses' => 'AuthController@logout'));
@@ -27,5 +29,3 @@ Route::group(array('before' => 'auth', 'prefix' => 'admin'), function()
 
 	Route::resource('post', 'PostsController', array('except' => array('show')));
 });
-
-Route::resource('blog', 'BlogController', array('only' => array('index', 'show')));
