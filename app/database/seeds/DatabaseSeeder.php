f13b02910f38fe416d92f9bf078b5bd2bc3dfa66
Dries Vints <dries.vints@gmail.com>
2013-07-02 18:36:44 +0200

    Add PostsTableSeeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('UsersTableSeeder');
+		$this->call('PostsTableSeeder');
 	}
 
 }
---
+++ app/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Models\Post;
+
+class PostsTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('posts')->truncate();
+
+		Post::create(array(
+			'title' => 'Foo',
+			'slug' => 'foo',
+			'body' => 'This is some lorum ipsum text.',
+		));
+
+		Post::create(array(
+			'title' => 'Bar',
+			'slug' => 'bar',
+			'body' => 'This is some lorum ipsum text.',
+		));
+
+		Post::create(array(
+			'title' => 'Baz',
+			'slug' => 'baz',
+			'body' => 'This is some lorum ipsum text.',
+		));
+	}
+
+}
