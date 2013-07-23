ea58ed06086886e12953f357754944f06d3ad189
Dries Vints <dries.vints@gmail.com>
2013-07-23 19:47:01 +0200

    Rework routes
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- app/routes.php
+++ app/routes.php
@@ -7,7 +7,8 @@
 */
 
 Route::model('posts', 'Models\\Post');
-Route::bind('blog', function($value)
+Route::model('pages', 'Models\\Page');
+Route::bind('post_slug', function($value)
 {
 	$posts = get_posts();
 
@@ -15,7 +16,7 @@ Route::bind('blog', function($value)
 
 	App::abort(404);
 });
-Route::bind('pages', function($value)
+Route::bind('page_slug', function($value)
 {
 	$pages = get_pages();
 
@@ -33,7 +34,7 @@ Route::bind('pages', function($value)
 Route::get('/', array('as' => 'home', 'uses' => 'Controllers\\HomeController@index'));
 
 Route::get('blog', array('as' => 'blog.index', 'uses' => 'Controllers\\BlogController@index'));
-Route::get('blog/{blog}', array('as' => 'blog.show', 'uses' => 'Controllers\\BlogController@show'));
+Route::get('blog/{post_slug}', array('as' => 'blog.show', 'uses' => 'Controllers\\BlogController@show'));
 
 Route::get('login', array('as' => 'login', 'uses' => 'Controllers\\AuthController@getLogin'));
 Route::post('login', 'Controllers\\AuthController@postLogin');
@@ -47,4 +48,4 @@ Route::group(array('before' => 'auth', 'prefix' => 'admin'), function()
 	Route::resource('pages', 'Controllers\\Admin\\PagesController', array('except' => array('show')));
 });
 
-Route::get('{pages}', array('as' => 'pages.show', 'uses' => 'Controllers\\PagesController@show'));
+Route::get('{page_slug}', array('as' => 'pages.show', 'uses' => 'Controllers\\PagesController@show'));
