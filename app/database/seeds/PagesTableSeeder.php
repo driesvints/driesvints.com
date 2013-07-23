18ef056d53cf0674b93f7e597d236d174b9be028
Dries Vints <dries.vints@gmail.com>
2013-07-23 19:30:28 +0200

    Add PagesSeeder
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

---
+++ app/database/seeds/PagesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Models\Page;
+
+class PagesTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('pages')->truncate();
+
+		Page::create(array(
+			'title' => 'Foo',
+			'slug' => 'foo',
+			'body' => 'This is some lorum ipsum text.',
+		));
+
+		Page::create(array(
+			'title' => 'Bar',
+			'slug' => 'bar',
+			'body' => 'This is some lorum ipsum text.',
+		));
+
+		Page::create(array(
+			'title' => 'Baz',
+			'slug' => 'baz',
+			'body' => 'This is some lorum ipsum text.',
+		));
+	}
+
+}
