203b7ca48f212ebebc8711655c057bddd168af82
Dries Vints <dries.vints@gmail.com>
2013-08-01 08:02:37 +0200

    Bootstrapify the login form
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,17 +1,37 @@
-<h1>Login to your account</h1>
+@include('header')
 
-{{ Form::open(array('route' => 'login')) }}
+<div class="row">
+	<div class="col-lg-6 col-offset-3">
+		<h1>Login to your account</h1>
 
-<p>
-	{{ Form::label('email') }}
-	{{ Form::email('email') }}
-</p>
+		{{ Form::open(array('route' => 'login', 'class' => 'form-horizontal')) }}
 
-<p>
-	{{ Form::label('password') }}
-	{{ Form::password('password') }}
-</p>
+		<fieldset>
+			<div class="form-group">
+				{{ Form::label('email', null, array('class' => 'col-lg-2 control-label')) }}
+				<div class="col-lg-7">
+					{{ Form::email('email', null, array('class' => 'form-control')) }}
+				</div>
+			</div>
 
-{{ Form::submit() }}
+			<div class="form-group">
+				{{ Form::label('password', null, array('class' => 'col-lg-2 control-label')) }}
+				<div class="col-lg-7">
+					{{ Form::password('password', array('class' => 'form-control')) }}
 
-{{ Form::close() }}
+					<div class="checkbox">
+					  <label>
+					    <input type="checkbox"> Remember me
+					  </label>
+					</div>
+
+					{{ Form::submit(null, array('class' => 'btn btn-primary')) }}
+				</div>
+			</div>
+		</fieldset>
+
+		{{ Form::close() }}
+	</div>
+</div>
+
+@include('footer')
