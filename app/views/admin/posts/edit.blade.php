e429ac3e2c28ba540ce43ab58161cf07f2202ad5
Dries Vints <dries.vints@gmail.com>
2013-08-07 19:39:07 +0200

    Remove whitespaces
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- app/views/admin/posts/edit.blade.php
+++ app/views/admin/posts/edit.blade.php
@@ -14,13 +14,13 @@
 				{{ Form::text('title', null, array('class' => 'form-control')) }}
 				{{ $errors->first('title') }}
 			</div>
-		
+
 			<div class="form-group">
 				{{ Form::label('slug') }}
 				{{ Form::text('slug', null, array('class' => 'form-control')) }}
 				{{ $errors->first('slug') }}
 			</div>
-			
+
 			<div class="form-group">
 				{{ Form::label('body') }}
 				{{ Form::textarea('body', null, array('class' => 'form-control')) }}
@@ -35,13 +35,13 @@
 				{{ Form::select('status', $statuses, null, array('class' => 'form-control')) }}
 				{{ $errors->first('status') }}
 			</div>
-		
+
 			<div class="form-group">
 				{{ Form::label('published_at') }}
 				{{ Form::text('published_at', Input::old('published_at', date('Y-m-d H:i:s')), array('class' => 'form-control')) }}
 				{{ $errors->first('published_at') }}
 			</div>
-			
+
 			{{ HTML::linkRoute('admin.posts.index', 'Cancel', null, array('class' => 'btn btn-default')) }}
 			{{ Form::submit('Save post', array('class' => 'btn btn-primary')) }}
 		</fieldset>
