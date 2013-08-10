84b22c4828be4249d054ea5b28113cf20d3aca33
Dries Vints <dries.vints@gmail.com>
2013-08-10 16:17:43 +0200

    Add Google Analytics
    Signed-off-by: Dries Vints <dries.vints@gmail.com>

--- app/views/footer.blade.php
+++ app/views/footer.blade.php
@@ -12,5 +12,17 @@
 {{ HTML::script('assets/libraries/jquery-1.10.2.min.js') }}
 {{ HTML::script('assets/libraries/bootstrap/js/bootstrap.min.js') }}
 
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-18478762-2']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+
 </body>
 </html>
