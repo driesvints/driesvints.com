7e4c03e27a7b35959212fed3309c6513debccbdc
Dries Vints <dries.vints@gmail.com>
2013-10-04 12:04:49 +0200

    Correct anchor links on homepage with smooth scroll

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,7 @@
 <div id="header">
     <div class="container">
         <ul class="list-inline">
-        	<li><a href="#">Blog</a></li>
+        	<li><a href="#recent-posts">Blog</a></li>
         	<li class="second"><a href="#">Work</a></li>
         	<li class="third"><a href="#">About</a></li>
         	<li><a href="#">Contact</a></li>
@@ -20,14 +20,14 @@
 
         	<p><a class="btn btn-success btn-large" href="#">Available For Hire</a></p>
 
-        	<p><a class="next" href="#"><i class="icon-chevron-sign-down"></i></a></p>
+        	<p><a class="next" href="#recent-posts"><i class="icon-chevron-sign-down"></i></a></p>
         </div>
     </div>
 </div>
 
 <div id="posts" class="container">
 
-<h2>Recent Posts</h2>
+<h2 id="recent-posts">Recent Posts</h2>
 
 @include('posts.list')
 
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,4 +3,24 @@ $(function()
 	$('.datetimepicker').datetimepicker();
 
 	$("#header").backstretch("/assets/images/header.jpg");
+
+    /**
+     * Smooth scroll to anchor.
+     *
+     * @Source: http://css-tricks.com/snippets/jquery/smooth-scrolling/
+     */
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
