64dcde6f2ea02b49a2beab4e21df161a6afe2218
Dries Vints <dries.vints@gmail.com>
2014-04-02 23:44:21 +0200

    Move Google Analytics to header

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -41,6 +41,19 @@
         {{ HTML::script('assets/libraries/html5shiv.min.js') }}
         {{ HTML::script('assets/libraries/respond.min.js') }}
     <![endif]-->
+
+    <!-- Google Analytics -->
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-18478762-2']);
+        _gaq.push(['_trackPageview']);
+    
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
 </head>
 
 @if (is_home())
