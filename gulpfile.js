a7161bffce3ba3b403742bca4c0ab58ec0eac1cf
Dries Vints <dries.vints@gmail.com>
2014-09-28 12:56:05 +0200

    Update NPM dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
-    clean = require('gulp-clean'),
+    rimraf = require('gulp-rimraf'),
     concat = require('gulp-concat'),
     cache = require('gulp-cache');
 
@@ -26,14 +25,14 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('images', function() {
-  return gulp.src('./public/src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('./public/assets/images'));
+    return gulp.src('./public/src/images/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('./public/assets/images'));
 });
 
 gulp.task('clean', function() {
-  return gulp.src(['./public/assets/css', './public/assets/js', './public/assets/images'], { read: false })
-    .pipe(clean());
+    return gulp.src(['./public/assets/css', './public/assets/js', './public/assets/images'], { read: false })
+        .pipe(rimraf());
 });
 
 gulp.task('default', ['clean'], function () {
@@ -41,12 +40,12 @@ gulp.task('default', ['clean'], function () {
 });
 
 gulp.task('watch', function() {
-  // Watch .scss files.
-  gulp.watch('./public/src/styles/**/*.scss', ['styles']);
+    // Watch .scss files.
+    gulp.watch('./public/src/styles/**/*.scss', ['styles']);
 
-  // Watch .js files.
-  gulp.watch('./public/src/scripts/**/*.js', ['scripts']);
+    // Watch .js files.
+    gulp.watch('./public/src/scripts/**/*.js', ['scripts']);
 
-  // Watch image files.
-  gulp.watch('./public/src/images/**/*', ['images']);
+    // Watch image files.
+    gulp.watch('./public/src/images/**/*', ['images']);
 });
