776a3d2e1019415a4f5d44b5c68e4b4f2382cf83
Dries Vints <dries.vints@gmail.com>
2014-10-11 13:52:24 +0200

    Rename production env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment([
     'local' => ['homestead'],
-    'ocean' => ['driesvints-com'],
+    'production' => ['driesvints-com'],
 ]);
 
 /*
