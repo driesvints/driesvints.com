e3f7aebc6ae96ca2d97d10202cc6d2ee720c925b
Dries Vints <dries.vints@gmail.com>
2014-10-12 21:43:25 +0200

    Fix homepage meta descriptions

--- src/Http/controllers/HomeController.php
+++ src/Http/controllers/HomeController.php
@@ -27,9 +27,9 @@ class HomeController extends BaseController
     {
         $posts = $this->contentManager->get('posts')->published()->orderBy('date', 'desc')->take(3);
 
-        $metaDescription = 'Web Developer with a passion for open-source, community & Laravel.
+        $this->metaDescription = 'Web Developer with a passion for open-source, community & Laravel.
             Currently works at BeatSwitch in the city of Antwerp, Belgium.';
 
-        return $this->view('home', compact('posts', 'metaDescription'));
+        return $this->view('home', compact('posts'));
     }
 }
