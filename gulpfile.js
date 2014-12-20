c540a13e0e8b46e7490e7d9fa65d9cc529be3d7f
Dries Vints <dries.vints@gmail.com>
2014-12-20 20:55:51 +0100

    Separate cleanup tasks in Gulp bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,21 @@ var gulp = require('gulp'),
     del = require('del');
 
 // Compiler tasks
-gulp.task('styles', ['clean:styles'], function () {
+gulp.task('styles', function () {
     return gulp.src('./public/src/styles/styles.scss')
         .pipe(sass({ style: 'compressed', container: './app/storage/tmp' }))
         .pipe(autoprefixer('last 15 versions'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./public/assets/css'));
 });
-gulp.task('scripts', ['clean:scripts'], function() {
+gulp.task('scripts', function() {
     return gulp.src('./public/src/scripts/**/*.js')
         .pipe(concat('script.js'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .pipe(gulp.dest('./public/assets/js'));
 });
-gulp.task('images', ['clean:images'], function() {
+gulp.task('images', function() {
     return gulp.src('./public/src/images/**/*')
         .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
         .pipe(gulp.dest('./public/assets/images'));
@@ -42,6 +42,10 @@ gulp.task('clean:images', function(cb) {
 
 // General tasks
 gulp.task('default', function () {
+    gulp.start('clean', 'compile');
+});
+
+gulp.task('compile', function () {
     gulp.start('styles', 'scripts', 'images');
 });
 
@@ -49,4 +53,8 @@ gulp.task('watch', function() {
     gulp.watch('./public/src/styles/**/*.scss', ['styles']);
     gulp.watch('./public/src/scripts/**/*.js', ['scripts']);
     gulp.watch('./public/src/images/**/*', ['images']);
+});
+
+gulp.task('clean', function () {
+    gulp.start('clean:styles', 'clean:scripts', 'clean:images');
 });
