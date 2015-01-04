b0ddb0fd584423dc0a8f8f367dfe71240b66dc14
Dries Vints <dries.vints@gmail.com>
2015-01-04 21:06:45 +0100

    Switch about and recent posts

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,8 +10,18 @@
         <a href="#about">About</a>
     </p>
 
-    <a class="next icon-highlight" href="#recent-posts"><i class="fa fa-chevron-circle-down"></i></a>
+    <a class="next icon-highlight" href="#about"><i class="fa fa-chevron-circle-down"></i></a>
 </div>
+
+<div id="about">
+    <img class="img-circle" src="http://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5.jpg?s=125" alt="">
+    <h3>Dries Vints</h3>
+    <p>
+        Maintainer of <a href="http://laravel.io">Laravel.IO</a>, the <a href="http://laravel.com">Laravel</a> Community Platform. Creator of <a href="https://github.com/BeatSwitch/lock">Lock</a>, a popular PHP Acl package. Lead Developer at <a href="http://beatswitch.com">BeatSwitch</a>.
+    </p>
+</div>
+
+<hr class="home-separator" />
 
 <div id="recent-posts">
     @if (count($posts))
@@ -22,16 +32,6 @@
 
     <p class="blog-btn">
         <a class="btn btn-default btn-lg btn-outline" href="{{ route('blog') }}">Read More</a>
-    </p>
-</div>
-
-<hr class="home-separator" />
-
-<div id="about">
-    <img class="img-circle" src="http://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5.jpg?s=125" alt="">
-    <h3>Dries Vints</h3>
-    <p>
-        Maintainer of <a href="http://laravel.io">Laravel.IO</a>, the <a href="http://laravel.com">Laravel</a> Community Platform. Creator of <a href="https://github.com/BeatSwitch/lock">Lock</a>, a popular PHP Acl package. Lead Developer at <a href="http://beatswitch.com">BeatSwitch</a>.
     </p>
 </div>
 
