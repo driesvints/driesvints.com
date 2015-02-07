6c786866cd83a0a9eec6cdb1b5c4ef9ca0aa7eef
Dries Vints <dries.vints@gmail.com>
2015-02-07 14:51:58 +0100

    Small typo

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -20,7 +20,7 @@
     <img class="img-circle" src="http://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5.jpg?s=125" alt="">
     <h3>Dries Vints</h3>
     <p>
-        Maintainer of <a href="http://laravel.io">Laravel.IO</a>, the <a href="http://laravel.com">Laravel</a> Community Platform. Creator of <a href="https://github.com/BeatSwitch/lock">Lock</a>, a popular PHP Acl package. Lead Developer at <a href="http://beatswitch.com">BeatSwitch</a>.
+        Maintainer of <a href="http://laravel.io">Laravel.IO</a>, the <a href="http://laravel.com">Laravel</a> Community Platform. Creator of <a href="https://github.com/BeatSwitch/lock">Lock</a>, a popular PHP acl package. Lead Developer at <a href="http://beatswitch.com">BeatSwitch</a>.
     </p>
 </div>
 
