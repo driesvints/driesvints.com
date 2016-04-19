a51096b341fe6d372c03f095c1bf52daadfcc626
Dries Vints <dries.vints@gmail.com>
2016-04-19 23:26:02 +0200

    Clean up blog page

--- source/blog.blade.php
+++ source/blog.blade.php
@@ -5,17 +5,9 @@
 <div id="blog">
     <h1 class="title">Blog</h1>
 
-    @php($firstPost = $posts->first())
-
-    <h3>{{ $year = $firstPost->publishedAt()->format('Y') }}</h3>
-
     @foreach ($posts as $post)
-        @if ($post->publishedAt()->format('Y') !== $year )
-            <h3>{{ $year = $post->publishedAt()->format('Y') }}</h3>
-        @endif
-
         <div class="post">
-            <div class="date">{{ $firstPost->publishedAt()->format('F j') }}</div>
+            <div class="date">{{ $post->publishedAt()->format('F j, Y') }}</div>
             <div class="title">
                 <a href="/blog/{{ $post->slug() }}">{{ $post->title() }}</a>
             </div>
