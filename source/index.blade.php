1836ecebf60bb01d6b39536e54be512e96cd18ff
Dries Vints <dries.vints@gmail.com>
2017-07-09 15:34:42 +0200

    Updates

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,16 +1,18 @@
 @include('_partials/header', ['metaDescription' => strip_tags($page->bio)])
 
 <div id="header">
-    <h1>Dries Vints</h1>
-    <h2>Web Developer</h2>
-    <p id="about">{!! $page->bio !!}</p>
-    <p class="social-media">
-        <a href="https://twitter.com/driesvints"><i class="fa fa-twitter"></i></a>
-        <a href="https://github.com/driesvints"><i class="fa fa-github"></i></a>
-        <a href="https://medium.com/@driesvints"><i class="fa fa-medium"></i></a>
-        <a href="https://www.linkedin.com/in/driesvints/"><i class="fa fa-linkedin"></i></a>
-        <a href="mailto:dries.vints@gmail.com"><i class="fa fa-paper-plane"></i></a>
-    </p>
+    <div class="about">
+        <h1>Dries Vints</h1>
+        <h2>Web Developer</h2>
+        <p class="bio">{!! $page->bio !!}</p>
+        <p class="social-media">
+            <a href="https://twitter.com/driesvints"><i class="fa fa-twitter"></i></a>
+            <a href="https://github.com/driesvints"><i class="fa fa-github"></i></a>
+            <a href="https://medium.com/@driesvints"><i class="fa fa-medium"></i></a>
+            <a href="https://www.linkedin.com/in/driesvints/"><i class="fa fa-linkedin"></i></a>
+            <a href="mailto:dries.vints@gmail.com"><i class="fa fa-paper-plane"></i></a>
+        </p>
+    </div>
 </div>
 
 @include('_partials/footer')
