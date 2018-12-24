642e0116cd6a01be16c581d83b002ab1b9105748
Dries Vints <dries.vints@gmail.com>
2018-12-24 13:32:41 +0100

    Add bio

--- source/index.blade.php
+++ source/index.blade.php
@@ -10,6 +10,7 @@
             <p class="bio">
                 I work for <a href="https://laravel.com">Laravel</a>,
                 maintain <a href="https://laravel.io">Laravel.io</a>
+                and organise <a href="https://fullstackbelgium.be">Full Stack Belgium</a>
                 and <a href="https://fullstackeurope.com">Full Stack Europe</a>.
             </p>
             <p class="social-media">
