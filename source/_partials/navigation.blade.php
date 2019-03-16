f848f350c268b1d0dc7cc478fde42033473d2202
Dries Vints <dries.vints@gmail.com>
2019-03-16 16:58:37 +0100

    Remove subtitle in nav

--- source/_partials/navigation.blade.php
+++ source/_partials/navigation.blade.php
@@ -4,7 +4,6 @@
             <img src="https://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5?s=100" width="50" class="rounded-full enlarge">
         </a>
         <p class="font-bold mb-0">Dries Vints</p>
-        <p class="mb-1">Software Engineer</p>
         <p class="md:text-xs">
             <a href="/">Home</a> |
             <a href="/blog">Blog</a> |
