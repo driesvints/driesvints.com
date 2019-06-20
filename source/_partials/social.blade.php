2a4dd02fc5f8c5bdf0ae7847eaf0f5332b534017
Dries Vints <dries.vints@gmail.com>
2019-06-21 00:09:56 +0200

    Re-arrange social icons

--- source/_partials/social.blade.php
+++ source/_partials/social.blade.php
@@ -1,27 +1,27 @@
 <ul class="text-2xl">
-    <li class="inline-block">
-        <a class="mr-6 sm:mr-10" href="/">
+    <li class="inline-block mr-6 sm:mr-10">
+        <a href="/">
             <i class="enlarge fas fa-globe-europe"></i>
         </a>
     </li>
-    <li class="inline-block">
-        <a class="mr-6 sm:mr-10" href="https://twitter.com/driesvints" target="_blank">
+    <li class="inline-block mr-6 sm:mr-10">
+        <a href="/blog">
+            <i class="enlarge fas fa-rss"></i>
+        </a>
+    </li>
+    <li class="inline-block mr-6 sm:mr-10">
+        <a href="https://twitter.com/driesvints" target="_blank">
             <i class="enlarge fab fa-twitter"></i>
         </a>
     </li>
-    <li class="inline-block">
-        <a class="mr-6 sm:mr-10" href="https://github.com/driesvints" target="_blank">
+    <li class="inline-block mr-6 sm:mr-10">
+        <a href="https://github.com/driesvints" target="_blank">
             <i class="enlarge fab fa-github"></i>
         </a>
     </li>
-    <li class="inline-block">
-        <a class="mr-6 sm:mr-10" href="https://www.linkedin.com/in/driesvints" target="_blank">
+    <li class="inline-block sm:mr-10">
+        <a href="https://www.linkedin.com/in/driesvints" target="_blank">
             <i class="enlarge fab fa-linkedin-in"></i>
-        </a>
-    </li>
-    <li class="inline-block">
-        <a class="sm:mr-10" href="/blog">
-            <i class="enlarge fas fa-rss"></i>
         </a>
     </li>
 </ul>
