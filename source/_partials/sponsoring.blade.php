92c1dd629c86cd617ea38e8b19c5a5c6e31ce196
Dries Vints <dries.vints@gmail.com>
2020-04-06 10:22:41 +0200

    Add Wishlistr

--- source/_partials/sponsoring.blade.php
+++ source/_partials/sponsoring.blade.php
@@ -3,13 +3,19 @@
         Do you like what I do or did you ever found something that I made useful? Consider supporting me!
     </p>
 
-    <p class="mb-4">
+    <p class="mb-0">
         <a class="btn text-sm mb-4" href="https://github.com/sponsors/driesvints" target="_blank">
             <i class="fab fa-github mr-1"></i> Support me regularly
         </a>
 
         <a class="btn text-sm" href="https://paypal.me/driesvints" target="_blank">
             <i class="fab fa-paypal mr-1"></i> Donate just once
+        </a>
+    </p>
+
+    <p class="mb-4 text-sm">
+        <a href="https://www.wishlistr.com/driesvints">
+            Or gift something from my Wishlistr <i class="fas fa-gift"></i>
         </a>
     </p>
 </div>
