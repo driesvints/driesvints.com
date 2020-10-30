8b30b93ba61adb80a6ea1e77204130befa6fec55
Dries Vints <dries@vints.io>
2020-10-30 09:14:21 +0100

    casing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -88,7 +88,7 @@
                         + <a href="https://eventy.io">Eventy</a>, a new way to organise events.<br>
                         + <a href="https://github.com/driesvints/dotfiles">Dotfiles</a>, my preferred way to set up my Mac.<br>
                         + <a href="https://github.com/EventSaucePHP/LaravelEventSauce">Laravel EventSauce</a>, event sourcing for Laravel apps.<br>
-                        + <a href="https://fullstackbelgium.be">Full Stack Belgium</a>, Events in Belgium for web developers.<br>
+                        + <a href="https://fullstackbelgium.be">Full Stack Belgium</a>, events in Belgium for web developers.<br>
                         + <a href="https://github.com/blade-ui-kit/blade-icons">Blade Icons</a>, easily use SVG icons in your Laravel Blade views.
                     </p>
 
