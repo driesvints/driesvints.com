0ec9c2f429e99f259ed3e3347f875b1ce31fd91c
Shift <shift@laravelshift.com>
2022-03-20 14:45:27 +0000

    Remove unnecessary `$model` property

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class PostFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Post::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
