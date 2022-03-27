a0773ad804cadd96437a7aeb9fb942c7ae1718fb
Dries Vints <dries@vints.io>
2022-03-27 21:11:03 +0200

    New about section

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,25 +1,43 @@
-<div id="about" class="max-w-3xl mx-auto px-6 py-10 sm:py-20">
-    <div class="text-center mb-6">
-        <a href="https://twitter.com/driesvints">
-            <img src="https://www.gravatar.com/avatar/e8321183acdf47a9ce838afd13a964b5?s=180" width="90" class="rounded-full enlarge inline-block mb-4 sm:mb-0 sm:mr-4" alt="">
-        </a>
+<div id="about" class="relative bg-white overflow-hidden">
+    <div class="max-w-7xl mx-auto">
+        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
+                <polygon points="50,0 100,0 50,100 0,100" />
+            </svg>
+
+            <div class="pt-1"></div>
+
+            <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div class="sm:text-center lg:text-left">
+                    <h2 class="mt-6 text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
+                        About me
+                    </h2>
+
+                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                        I'm a software engineer from Belgium. I work as one of the core team members of <a
+                            href="https://laravel.com">Laravel</a>, the popular PHP framework.
+                    </p>
+
+                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                        My passions are <a href="https://github.com/driesvints">open-source</a>, building communities, and writing testable & readable code.
+                    </p>
+
+                    <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                        Together with my buddies <a href="https://twitter.com/riasvdv">Rias</a> and <a
+                            href="https://twitter.com/freekmurze">Freek</a> I organize events for <a
+                            href="https://fullstackbelgium.be">Full Stack Belgium</a> in the cities of <a
+                            href="https://meetup.com/fullstackantwerp">Antwerp</a> and <a
+                            href="https://meetup.com/fullstackghent">Ghent</a>. And we also organise <a
+                            href="https://fullstackeurope.com">Full Stack Europe</a>, the conference for the whole team.
+                    </p>
+                </div>
+            </main>
+        </div>
     </div>
-
-    <h2 class="text-4xl text-center font-bold mb-8">About Me</h2>
-
-    <p class="mb-4">
-        I'm a software engineer from Antwerp, Belgium. I work as one of the core team members of <a href="https://laravel.com">Laravel</a>, the popular PHP framework. I'm also the creator of <a href="https://blade-ui-kit.com">Blade UI Kit</a>, a set of renderless components to utilise in your Laravel Blade views.
-    </p>
-
-    <p class="mb-4">
-        My passions are <a href="https://github.com/driesvints">open-source</a>, building communities, managing software teams, and creating quality and maintainable products.
-    </p>
-
-    <p class="mb-4">
-        Together with my buddies <a href="https://twitter.com/riasvdv">Rias</a> and <a href="https://twitter.com/freekmurze">Freek</a> I organize events for <a href="https://fullstackbelgium.be">Full Stack Belgium</a> in the cities of <a href="https://meetup.com/fullstackantwerp">Antwerp</a> and <a href="https://meetup.com/fullstackghent">Ghent</a>. And we also organise <a href="https://fullstackeurope.com">Full Stack Europe</a>, the conference for the whole team.
-    </p>
-
-    <p>
-        Follow me on Twitter: <a href="https://twitter.com/driesvints" target="_blank" rel="noopener">@driesvints</a>.
-    </p>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src="{{ asset('images/about.jpeg') }}"
+            alt="">
+    </div>
 </div>
