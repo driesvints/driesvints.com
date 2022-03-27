fc983e616227caf48538a76ad5aa0edbd6ed56d6
Dries Vints <dries@vints.io>
2022-03-27 21:25:56 +0200

    wip

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -36,7 +36,7 @@
         </div>
     </div>
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        <img class="h-56 w-full object-cover object-top sm:h-72 md:h-96 lg:w-full lg:h-full"
             src="{{ asset('images/about.jpeg') }}"
             alt="">
     </div>
