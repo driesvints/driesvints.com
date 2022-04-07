3f848e7ec56ece3126ec78c58a286ba67b456634
Dries Vints <dries@vints.io>
2022-04-07 11:47:02 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,10 +86,8 @@
                 <div class="text-center">
                     <p class="text-sm sm:text-base text-gray-600 mb-12">
                         + <a href="https://github.com/driesvints/dotfiles">Dotfiles</a>, my preferred way to set up my Mac.<br>
-                        + <a href="https://github.com/EventSaucePHP/LaravelEventSauce">Laravel EventSauce</a>, event sourcing for Laravel apps.<br>
                         + <a href="https://fullstackbelgium.be">Full Stack Belgium</a>, events in Belgium for web developers.<br>
                         + <a href="https://github.com/blade-ui-kit/blade-icons">Blade Icons</a>, easily use SVG icons in your Laravel Blade views.<br>
-                        + <a href="https://github.com/github-php/sponsors">PHP GitHub Sponsors</a>, a package for PHP to interact with GitHub Sponsors.<br>
                         + <a href="https://github.com/driesvints/vat-calculator">VatCalculator</a>, handle all the hard stuff related to EU MOSS tax/vat regulations.
                     </p>
                 </div>
