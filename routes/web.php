bf88f2e7866a7cdec7b3cffc21ec8d93a19033b9
Dries Vints <dries@vints.io>
2022-05-05 20:35:53 +0200

    Add favicon

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)->name('home');
+Route::view('/', 'home')->name('home');
 Route::redirect('/blog', '/')->name('blog');
 Route::get('/blog/{post:slug}', PostController::class)->name('post');
