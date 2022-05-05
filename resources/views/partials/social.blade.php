bce346eace33ea376a8d35dcbfd5827766c2a95b
Dries Vints <dries@vints.io>
2022-05-05 20:45:40 +0200

    wip

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,13 +1,13 @@
 <ul class="text-2xl">
     @auth
-        <li class="inline-block mr-6 sm:mr-10">
+        <li class="inline-block mr-6">
             <a href="{{ url('/nova/resources/posts') }}" aria-label="Nova">
                 <x-fas-user-shield class="enlarge w-6 h-6" />
             </a>
         </li>
     @endauth
 
-    <li class="inline-block mr-6 sm:mr-10">
+    <li class="inline-block mr-6">
         <a href="https://twitter.com/driesvints" target="_blank" rel="noopener" aria-label="Twitter">
             <x-fab-twitter class="enlarge w-6 h-6" />
         </a>
