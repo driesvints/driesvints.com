3f46e6cf8e11dcf5965e4b51fcfa2f08acedc22d
Dries Vints <dries@vints.io>
2022-11-15 09:34:12 +0100

    wip

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -12,6 +12,11 @@
             <x-fab-twitter class="enlarge w-6 h-6" />
         </a>
     </li>
+    <li class="hidden inline-block mr-6">
+        <a rel="me" href="https://phpc.social/@driesvints" target="_blank" aria-label="Mastodon">
+            <x-fab-mastodon class="enlarge w-6 h-6" />
+        </a>
+    </li>
     <li class="inline-block">
         <a href="https://github.com/driesvints" target="_blank" rel="noopener" aria-label="Github">
             <x-fab-github class="enlarge w-6 h-6" />
