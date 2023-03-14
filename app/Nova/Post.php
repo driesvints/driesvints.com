ad6fea2aa83525e365af91697c64d09648b54429
driesvints <driesvints@users.noreply.github.com>
2023-03-14 22:32:08 +0000

    Fix code styling

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -38,7 +38,6 @@ final class Post extends Resource
     /**
      * Get the fields displayed by the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function fields(Request $request)
@@ -72,7 +71,6 @@ final class Post extends Resource
     /**
      * Get the cards available for the request.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function cards(Request $request)
@@ -83,7 +81,6 @@ final class Post extends Resource
     /**
      * Get the filters available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function filters(Request $request)
@@ -94,7 +91,6 @@ final class Post extends Resource
     /**
      * Get the lenses available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function lenses(Request $request)
@@ -105,7 +101,6 @@ final class Post extends Resource
     /**
      * Get the actions available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function actions(Request $request)
