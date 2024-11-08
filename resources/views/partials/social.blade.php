7fff9d3b22ba76c9d7eec81ba626d6d187ce6aae
Dries Vints <dries@vints.be>
2024-11-08 21:49:59 +0100

    Add Bluesky

--- resources/views/partials/social.blade.php
+++ resources/views/partials/social.blade.php
@@ -1,22 +1,27 @@
 <ul class="text-2xl">
     <li class="inline-block mr-6">
         <a href="https://twitter.com/driesvints" target="_blank" rel="noopener" aria-label="Twitter">
-            <x-fab-x-twitter class="enlarge w-6 h-6" />
+            <x-si-x class="enlarge w-6 h-6" />
+        </a>
+    </li>
+    <li class="inline-block mr-6">
+        <a rel="me" href="https://bsky.app/profile/driesvints.com" target="_blank" aria-label="Bluesky">
+            <x-si-bluesky class="enlarge w-6 h-6" />
         </a>
     </li>
     <li class="inline-block mr-6">
         <a rel="me" href="https://phpc.social/@driesvints" target="_blank" aria-label="Mastodon">
-            <x-fab-mastodon class="enlarge w-6 h-6" />
+            <x-si-mastodon class="enlarge w-6 h-6" />
         </a>
     </li>
     <li class="inline-block mr-6">
         <a href="https://github.com/driesvints" target="_blank" rel="noopener" aria-label="Github">
-            <x-fab-github class="enlarge w-6 h-6" />
+            <x-si-github class="enlarge w-6 h-6" />
         </a>
     </li>
     <li class="inline-block">
         <a href="https://linkedin.com/in/driesvints" target="_blank" rel="noopener" aria-label="LinkedIn">
-            <x-fab-linkedin-in class="enlarge w-6 h-6" />
+            <x-si-linkedin class="enlarge w-6 h-6" />
         </a>
     </li>
 </ul>
